5c1e19c4b5a1069630ab03dc695ce3ab8bc8edf6
Jacob Henning <jacobshenning@gmail.com>
2017-07-02 17:51:32 -0700

    Testing Update

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
 <?php
 
-# Setup
-
-require 'vendor/AltoRouter.php';
-
-# Initiate
-
-$router_path = '';
-$router = new AltoRouter();
-
-# Routes
-
-$router->map( 'GET', $router_path . '/token', function() {
-  require 'token.php';
-});
-
-# Calculate
-
-$match = $router->match();
-
-if ( $match && is_callable( $match['target'] ) ) {
-	call_user_func_array( $match['target'], $match['params'] );
-} else {
-	echo '404';
-}
+echo "Hello World";
 
 ?>
