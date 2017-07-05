073729100fd81b8a98431f3a793c0dae03cda041
Jacob Henning <jacobshenning@gmail.com>
2017-07-05 13:16:18 -0700

    0Auth Integration 1

---
+++ app/setup/0auth2.php
@@ -0,0 +1,24 @@
+<?php
+
+// Define Credentials
+$dsn      = 'mysql:dbname=' . $db_name . ';host=' . $db_host;
+$username = $db_user;
+$password = $db_pass;
+
+// Include 0auth2
+require_once('../private/vendor/OAuth2/Autoloader.php');
+OAuth2\Autoloader::register();
+
+// Setup Database
+$storage = new OAuth2\Storage\Pdo(array('dsn' => $dsn, 'username' => $username, 'password' => $password));
+
+// Pass a storage object or array of storage objects to the OAuth2 server class
+$server = new OAuth2\Server($storage);
+
+// Add the "Client Credentials" grant type (it is the simplest of the grant types)
+$server->addGrantType(new OAuth2\GrantType\ClientCredentials($storage));
+
+// Add the "Authorization Code" grant type (this is where the oauth magic happens)
+$server->addGrantType(new OAuth2\GrantType\AuthorizationCode($storage));
+
+?>
--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ *  file: index.php
+ *  path: /
+ */
+
+// error reporting (this is a demo, after all!)
+ini_set('display_errors',1);error_reporting(E_ALL);
+
+require '../app/setup/credentials.php';
+require '../app/setup/0auth2.php';
+
 echo 'Hello World';
 
 ?>
