27b77aaa416c39221bf2170d27c8920a607dcb3e
Jacob Henning <jacobshenning@gmail.com>
2017-07-05 17:16:47 -0700

    Resource Controller

--- app/setup/routes.php
+++ app/setup/routes.php
@@ -30,6 +30,20 @@ $router->map( 'POST', '/api/token', function() {
 	$server->handleTokenRequest(OAuth2\Request::createFromGlobals())->send();
 });
 
+$router->map( 'POST', '/api/controller', function() {
+
+	// include our OAuth2 Server object
+	require_once __DIR__.'/server.php';
+
+	// Handle a request to a resource and authenticate the access token
+	if (!$server->verifyResourceRequest(OAuth2\Request::createFromGlobals())) {
+	    $server->getResponse()->send();
+	    die;
+	}
+
+	echo json_encode(array('success' => true, 'message' => 'You accessed my APIs!'));
+});
+
 // match current request url
 $match = $router->match();
 
